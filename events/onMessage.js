d57a3c8d642c513df1078105f9a382ab2e6441e9
Anton Shevchuk <anton.shevchuk@gmail.com>
2024-05-07 16:21:54 +0200

    Small code style fix

--- events/onMessage.js
+++ events/onMessage.js
@@ -26,7 +26,7 @@ function onMessage(event) {
     }
   } else {
     // If the Chat app doesn't detect a slash command
-    let name = ''
+    let name
 
     if (event.space.type === 'DM') {
       name = 'You'
