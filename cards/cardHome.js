3a8d968750b273f521fc17d06cc4b58b0c1e92f8
Anton Shevchuk <anton.shevchuk@gmail.com>
2024-05-17 16:17:53 +0200

    Updated links

--- cards/cardHome.js
+++ cards/cardHome.js
@@ -1,24 +1,91 @@
-function cardHome() {
+function cardHome () {
   return {
-    sections: [
+    'header': {
+      'title': 'Google Chat Bot Development',
+      'subtitle': 'Explore detailed guides in English and Ukrainian',
+      'imageUrl': 'https://raw.githubusercontent.com/AntonShevchuk/bender-2.0/main/assets/bender_128x128.png'
+    },
+    'sections': [
       {
-        widgets: [
+        'header': 'Articles in English',
+        'widgets': [
           {
-            buttonList: {
-              buttons: [
-                {
-                  text: 'Manual for developers',
-                  onClick: {
-                    openLink: {
-                      url: 'https://anton.shevchuk.name/google/google-chat-bot-first-steps/'
-                    }
-                  }
-                }
-              ]
+            'textParagraph': {
+              'text': '— <a href="https://medium.com/@AntonShevchuk/google-chat-bot-first-steps-d051bc67d6b9">First Steps</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://medium.com/@AntonShevchuk/google-chat-bot-create-a-dialog-1f397cb2d7cd">Create a Dialog</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://medium.com/@AntonShevchuk/google-chat-bot-related-dialogs-9fee9db2ca14">Related Dialogs</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://medium.com/@AntonShevchuk/google-chat-bot-properties-f9eae0d2e277">Properties</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://medium.com/@AntonShevchuk/google-chat-bot-interaction-and-card-updates-e2d5dc995f78">Interaction and Card Updates</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://medium.com/@AntonShevchuk/google-chat-bot-integrate-jira-part-i-ab0c63b98850">Integrate JIRA Part I</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://medium.com/@AntonShevchuk/google-chat-bot-integrate-jira-part-two-5141d7aeca4b">Integrate JIRA Part II</a>'
+            }
+          }
+        ]
+      },
+      {
+        'header': 'Статті українською',
+        'widgets': [
+          {
+            'textParagraph': {
+              'text': '— <a href="https://anton.shevchuk.name/google/google-chat-bot-first-steps/">Перші кроки</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://anton.shevchuk.name/google/google-chat-bot-create-first-dialog/">Створіть перший діалог</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://anton.shevchuk.name/google/google-chat-bot-sequential-dialogs/">Послідовні діалоги</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://anton.shevchuk.name/google/google-chat-bot-properties/">Властивості</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://anton.shevchuk.name/google/google-chat-bot-interactions-and-card-updates/">Взаємодії та оновлення карток</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://anton.shevchuk.name/google/google-chat-bot-jira-integration-part-one/">Інтеграція JIRA Частина I</a>'
+            }
+          },
+          {
+            'textParagraph': {
+              'text': '— <a href="https://anton.shevchuk.name/google/google-chat-bot-jira-integration-part-two/">Інтеграція JIRA Частина II</a>'
             }
           }
         ]
       }
     ]
-  };
+  }
 }
